0e73a91aab18af14930db205a7e00acb7b3a70ea
Max Mowbray <maxmowbray@msn.com>
2021-11-15 11:37:14 +0000

    Update __init__.py

--- ps_gym/envs/__init__.py
+++ ps_gym/envs/__init__.py
@@ -1,2 +1,3 @@
 from ps_gym.envs.singlestageparallel import SingleStageParallelMaster, SingleStageParallelSO1
 from ps_gym.envs.singlestageparallel_large import SingleStageParallelLMaster
+from ps_gym.envs.singlestageparallel_largestateexp import SingleStageParallelLStateExp
